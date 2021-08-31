60316ccadff23cb78f30217d1643a0ed635459ed
NikolaGanchev <nikolamihailovg@gmail.com>
2021-08-31 14:42:43 +0300

    Added multiline to single line transformations to aid in unit creation process (such as translating multiple lines of code to a single line translation string)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import tokenizer.Token;
 import tokenizer.Tokenizer;
 import transformers.DocumentToPrettyStringTransformer;
 import transformers.ResultTransformer;
+import transformers.StringArrayListToSingleLine;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,12 +12,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static boolean multilineToSingleLine = false;
+
     public static void main(String[] args) {
-        Path pathToInput = Path.of("src/input.txt");
+        Path pathToInput = Path.of(multilineToSingleLine? "src/input_multiline_to_singleline.txt": "src/input.txt");
+
         Tokenizer tokenizer = new Tokenizer();
 
         try {
-            Files.lines(pathToInput).forEach(tokenizer::addString);
+            // Check if there is a multiline to singleline request
+            // That comes in handy when you have to put in code translations
+            if (multilineToSingleLine) {
+                ArrayList<String> lines = new ArrayList<>();
+                Files.lines(pathToInput).forEach(lines::add);
+
+                StringArrayListToSingleLine transformer = new StringArrayListToSingleLine();
+                System.out.println(transformer.transform(lines));
+                return;
+            } // Else start lesson building process
+            else {
+                Files.lines(pathToInput).forEach(tokenizer::addString);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/transformers/StringArrayListToSingleLine.java
@@ -0,0 +1,17 @@
+package transformers;
+
+import java.util.ArrayList;
+
+public class StringArrayListToSingleLine implements Transformer<String, ArrayList<String>> {
+    @Override
+    public String transform(ArrayList<String> lines) {
+        StringBuilder result = new StringBuilder();
+
+        for (String line : lines) {
+            result.append(line);
+            result.append("\\n");
+        }
+
+        return result.toString();
+    }
+}
